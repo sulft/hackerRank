d81ac0367af054e072aa90421fb848e6597ebf3e
terryvilver <vilver.terry@gmail.com>
2020-10-12 23:10:40 +0200

    récupérer des données entré par l'utilisateur

---
+++ java/stdin.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class stdin {
+    public static void main(String[] args) {
+        Scanner affiche = new Scanner(System.in);
+        int a = affiche.nextInt();
+        int b = affiche.nextInt();
+        int c = affiche.nextInt();
+
+        System.out.print("\n");
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
