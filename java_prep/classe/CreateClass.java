32701f6672d395a74030f76d989fc7dc36ec9e39
terryvilver <vilver.terry@gmail.com>
2020-10-21 09:16:21 +0200

    interchanger des villes

--- java_prep/classe/CreateClass.java
+++ java_prep/classe/CreateClass.java
@@ -1,13 +1,20 @@
 public class CreateClass {
     public static void main(String []args) {
-        Ville test = new Ville();
+        Ville test = new Ville("Nantes", "France", 213);
+        Ville tmp = new Ville();
         Ville test1 = new Ville("Paris","France",123);
 
-        System.out.println(test.getNomVille());
+        System.out.println("Le nom de la ville 1 est :" + test.getNomVille());
+        System.out.println("Le nom de la ville 2 est :" + test1.getNomVille());
 
-        test.setNomVille("Nantes");
+        //on interchange le nom des villes
+        tmp.setNomVille(test.getNomVille());
+        test.setNomVille(test1.getNomVille());
+        test1.setNomVille(tmp.getNomVille());
+        
+        System.out.println("Le nom de la ville 1 est :" + test.getNomVille());
+        System.out.println("Le nom de la ville 2 est :" + test1.getNomVille());
 
-        System.out.println(test.getNomVille());
 
     }
 }
