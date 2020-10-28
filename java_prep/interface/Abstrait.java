39cc62e3edcf1b5992dd494b2a291b755122e9fa
vilver <vilver.terry@gmail.com>
2020-10-29 00:25:22 +0100

    interface

---
+++ java_prep/interface/Abstrait.java
@@ -0,0 +1,7 @@
+public class Abstrait {
+    public static void main(String []args) {
+        Animal c = new Chien("bleu",56);
+        c.faireLeBeau();
+        System.out.println(c.toString());
+    }
+}
---
+++ java_prep/interface/Animal.java
@@ -0,0 +1,139 @@
+public abstract class Animal {
+    //attribut
+    protected String couleur;
+    protected int poids;
+
+    //ATTENTION LES CLASSES ABSTRAITES N'ONT PAS DE CONSTRUCTEUR
+
+    //methode 
+    public void manger() {
+        System.out.println("je mange de la viande.");
+    }
+
+    public void boire() {
+        System.out.println("je bois de l'eau.");
+    }
+    abstract void crier();
+    abstract void deplacement();
+}
+
+abstract class Canin extends Animal {
+    public void deplacement() {
+        System.out.println("Je me déplace seul !");
+    }
+    
+}
+
+abstract class Felin extends Animal {
+    public void deplacement() {
+        System.out.println("Je me déplace en groupe !");
+    } 
+} 
+
+class Loup extends Canin {
+    public Loup() {
+        this.couleur = "inconnu";
+        this.poids = 0;
+    }
+
+    public Loup(String couleur, int poids) {
+        this.couleur = couleur;
+        this.poids = poids;
+    }
+
+    public void crier() {
+        System.out.println("J'hurle lors de la pleine lune' !");
+    }
+
+    public String toString() {
+        return "je suis un " + this.getClass() + " de couleur " + this.couleur + " et je pèse " + this.poids + "kg.";
+    }
+} 
+
+class Chien extends Canin implements Rintintin {
+    public Chien() {
+        this.couleur = "inconnu";
+        this.poids = 0;
+    }
+
+    public Chien(String couleur, int poids) {
+        this.couleur = couleur;
+        this.poids = poids;
+    }
+
+    public void crier() {
+        System.out.println("J'aboie sur les visiteurs !");
+    }
+
+    public void faireLeBeau() {
+        System.out.println("Je suis un chien qui fait le beau.");
+    }
+
+    public String toString() {
+        return "je suis un " + this.getClass() + " de couleur " + this.couleur + " et je pèse " + this.poids + "kg.";
+    }
+} 
+
+class Lion extends Felin {
+    public Lion() {
+        this.couleur = "inconnu";
+        this.poids = 0;
+    }
+
+    public Lion(String couleur, int poids) {
+        this.couleur = couleur;
+        this.poids = poids;
+    }
+
+    public void crier() {
+        System.out.println("Je rugis!");
+    }
+
+    public String toString() {
+        return "je suis un " + this.getClass() + " de couleur " + this.couleur + " et je pèse " + this.poids + "kg.";
+    }
+} 
+
+class Tigre extends Felin {
+    public Tigre() {
+        this.couleur = "inconnu";
+        this.poids = 0;
+    }
+
+    public Tigre(String couleur, int poids) {
+        this.couleur = couleur;
+        this.poids = poids;
+    }
+
+    public void crier() {
+        System.out.println("Je grogne !");
+    }
+
+    public String toString() {
+        return "je suis un " + this.getClass() + " de couleur " + this.couleur + " et je pèse " + this.poids + "kg.";
+    }
+} 
+
+class Chat extends Felin {
+    public Chat() {
+        this.couleur = "inconnu";
+        this.poids = 0;
+    }
+
+    public Chat(String couleur, int poids) {
+        this.couleur = couleur;
+        this.poids = poids;
+    }
+
+    public void crier() {
+        System.out.println("Je miaule !");
+    }
+
+    public String toString() {
+        return "je suis un " + this.getClass() + " de couleur " + this.couleur + " et je pèse " + this.poids + "kg.";
+    }
+} 
+
+interface Rintintin {
+    public void faireLeBeau();
+}
