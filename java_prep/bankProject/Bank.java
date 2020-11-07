847ccdfbb9eff179a090eda78747161b8b4511e7
Terry Vilver <vilver.terry@gmail.com>
2020-11-07 11:23:30 +0100

    fin projet création banque

--- java_prep/bankProject/Bank.java
+++ java_prep/bankProject/Bank.java
@@ -4,29 +4,30 @@ import java.util.*;
 public class Bank {
     public static Scanner scan =new Scanner(System.in);
     public static void main(String []args) {
-        LaBanque b = new LaBanque();
-        b.depot();
-        System.out.println(b.toString());
-        b.retrait();
-        System.out.println(b.toString());
+        LaBanque test = new LaBanque(1000,"150","VILVER");
+        LaBanque test1 = new LaBanque(2000,"151","LEVY");
+        test.afficheMontant();
+        test1.afficheMontant();
+
+        test.transfertArgent(test1);
+
+        System.out.println(test1.toString());
 
     }
 }
-
 
 class LaBanque {
     /* ** ATTRIBUT ** */
     private int solde;
     private String id_client;
     private String nom_client;
-    private int montant;
+    private static int montant = 0;
 
     /* ** CONSTRUCTEUR ** */
     public LaBanque() {
         solde = 0;
         id_client = "###";
         nom_client = "Inconnu";
-        montant = 0;
 
         System.out.println("Compte client crée..\n");
     }
@@ -35,6 +36,8 @@ class LaBanque {
         this.solde = solde;
         this.id_client = id_client;
         this.nom_client = nom_client;
+
+        System.out.println("Compte client crée..\n");
     }
 
     /* ** Methode de classe ** */
@@ -54,6 +57,7 @@ class LaBanque {
         return Bank.scan.nextInt();
     }
 
+    /* ** Depot d'argent ** */
     public void depot() {
         System.out.print("Combien voulez-vous déposer (en euros) ? ");
 
@@ -61,20 +65,26 @@ class LaBanque {
 
         if(this.montant > 0) {
             this.solde = this.solde + this.montant;
-            System.out.println("Vous avez déposé " + this.montant + " euros.");
+            System.out.println("Vous avez déposé " + this.montant + " euros.\n");
         } else {
             System.out.println("Erreur dans la saisie..");
         }
     }
 
+    /* ** Retrait d'argent ** */
     public void retrait() {
         System.out.print("Combien voulez-vous retirer (en euros) ? ");
 
         this.montant = getScanI();
 
         if(this.montant > 0 && this.montant <= 700) {
-            this.solde = this.solde - this.montant;
-            System.out.println("Vous avez retiré " + this.montant + " euros.");
+            if (this.solde - this.montant < 0) {
+                System.out.println("Vous n'avez pas assez d'argent sur votre compte.");
+                System.out.println("Le solde de votre compte est de " + this.solde + " euros.\n");
+            } else {
+                this.solde = this.solde - this.montant;
+                System.out.println("Vous avez retiré " + this.montant + " euros.\n");
+            }
         } else if (this.montant > 700) {
             System.out.println("La limite de retrait max est de 700 euros !");
         } else {
@@ -82,9 +92,36 @@ class LaBanque {
         }
     }
 
+    public void afficheMontant() {
+        System.out.println("Vous ( " + this.nom_client + " ) avez actuellement " + this.solde + " euros dans votre compte.\n");
+    }
+
+    /* ** Transfert d'argent ** */
+    public void transfertArgent(LaBanque beneficier) {
+        System.out.print("Combien voulez-vous transférer à " + beneficier.nom_client  + " (en euros) ? ");
+
+        this.montant = getScanI();
+
+        if(this.montant > 0 && this.montant <= 700) {
+            if (this.solde - this.montant < 0) {
+                System.out.println("Vous n'avez pas assez d'argent sur votre compte.");
+                System.out.println("Le solde de votre compte est de " + this.solde + " euros.\n");
+            } else {
+                beneficier.solde = beneficier.solde + this.montant;
+                this.solde-=this.montant;
+                System.out.println("Le compte de "+ beneficier.nom_client + " à bénéficié de " + this.montant + " euros.\n");
+            }
+        } else if (this.montant > 700) {
+            System.out.println("La limite de transfert max est de 700 euros !");
+        } else {
+            System.out.println("Erreur dans la saisie..");
+        }
+    }
+
+    /* ** methode toString ** */
     public String toString() {
         return "Le propriétaire du compte est " + getNomClient() +
-        ". Son numéro de compte est " + getIdClient() + ". Le solde de son compte est : " +
-        getSolde() + " euros.\n";
+        ". \nSon numéro de compte est " + getIdClient() + ". \nLe solde de son compte est le : " +
+        getSolde() + " euros.";
     }
 }
