c4cfe25e87c8fefbde9278068a3b48b41f3846b4
vilver <vilver.terry@gmail.com>
2020-11-10 00:33:27 +0100

    Jframe herité

--- java_prep/gui/Gui.java
+++ java_prep/gui/Gui.java
@@ -1,18 +1,25 @@
 import javax.swing.*;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
+
 import java.awt.*;
 
 
 public class Gui extends JFrame {
     public Gui() {
-        
+        super("gui");
 
+        this.setTitle("Ma fenetre Java");
+        this.setSize(300,300);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        this.setVisible(true); //rends la fenêtre visible
     }
 
     public static void main(String []args) throws Exception {
         UIManager.setLookAndFeel( new NimbusLookAndFeel() );
-        JFrame Fenetre = new JFrame();
+        Gui test = new Gui();
+
     }
 }
