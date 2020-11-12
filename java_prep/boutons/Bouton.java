0679ccaf9c0dfe4ec138240f418a8321756fab7b
vilver <vilver.terry@gmail.com>
2020-11-12 23:52:51 +0100

    GridLayout

--- java_prep/boutons/Bouton.java
+++ java_prep/boutons/Bouton.java
@@ -14,13 +14,29 @@ public class Bouton extends JFrame {
 
         pan.add(bouton);
 
-        this.setLayout(new BorderLayout());//on définit le type de Layout à utiliser
+        //BorderLayout
+        /*this.setLayout(new BorderLayout());//on définit le type de Layout à utiliser
 
         this.getContentPane().add(new JButton("NORD"), BorderLayout.NORTH) ;
         this.getContentPane().add(new JButton("SUD"), BorderLayout.SOUTH);
         this.getContentPane().add(new JButton("MILLIEU"), BorderLayout.CENTER);
         this.getContentPane().add(new JButton("EST"), BorderLayout.EAST);
-        this.getContentPane().add(new JButton("OUEST"), BorderLayout.WEST);
+        this.getContentPane().add(new JButton("OUEST"), BorderLayout.WEST);*/
+        //this.setVisible(true);
+
+        //GridLayout
+        this.setLayout(new GridLayout(3,4));
+
+        this.getContentPane().add(new JButton("1"));
+        this.getContentPane().add(new JButton("2"));
+        this.getContentPane().add(new JButton("3"));
+        this.getContentPane().add(new JButton("4"));
+        this.getContentPane().add(new JButton("5"));
+        this.getContentPane().add(new JButton("6"));
+        this.getContentPane().add(new JButton("7"));
+        this.getContentPane().add(new JButton("8"));
+        this.getContentPane().add(new JButton("9"));
+        this.getContentPane().add(new JButton("10"));
         this.setVisible(true);
     }
     public static void main(String []args) {
