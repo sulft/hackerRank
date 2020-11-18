9dfc6869eebe1955b1070ebd987e17a716e571a9
vilver <vilver.terry@gmail.com>
2020-11-19 00:54:52 +0100

    création de deux boutons

--- java_prep/boutons/Bouton.java
+++ java_prep/boutons/Bouton.java
@@ -9,14 +9,14 @@ public class Bouton extends JFrame implements ActionListener{
     private JButton fenetre = new JButton("Le BOUTON");
     private GridLayout g = new GridLayout();
     private FlowLayout test = new FlowLayout(FlowLayout.CENTER,15,20);
-    private BorderLayout bLayout = new BorderLayout();
     private JLabel label = new JLabel();
     private JLabel label1 = new JLabel();
     private int compteur = 0;
+    private JButton bouton = new JButton("incrementation");
+    private JButton bouton2 = new JButton("decrementation");
 
     Bouton() {
         //Création de bouton
-        JButton bouton = new JButton("compteur");
 
         // Configuration de la fenetre
         this.setTitle("Les boutons");
@@ -26,24 +26,42 @@ public class Bouton extends JFrame implements ActionListener{
 
 
         //BorderLayout
-        pan.setLayout(bLayout);//on définit le type de Layout à utiliser
-        label.setText("Vous avez clique " + this.compteur + " fois");
+        pan.setLayout(new BorderLayout());//on définit le type de Layout à utiliser
+
+
+        //création d'un second JPanel
+        JPanel bottom = new JPanel();
+        bottom.add(bouton);
+        bottom.add(bouton2);
+        //ajout d'un JPanel dans un autre JPanel
+        pan.add(bottom, BorderLayout.SOUTH);
+
 
         //ajout du bouton a notre fenetre en tant que event
         bouton.addActionListener(this);
-        pan.add (bouton, BorderLayout.SOUTH);
+        bouton2.addActionListener(this);
 
+        //placement des éléments dans ça fenetre
+        label.setText("Vous avez clique " + this.compteur + " fois");
         label.setForeground (Color.red); //change la couleur d'écrit du texte
         label.setHorizontalAlignment(JLabel.CENTER); //aligne le texte horizontalement
         pan.add(label, BorderLayout.CENTER); //Place le texte au centre
-        pan.add(bouton, BorderLayout.SOUTH);
         this.setContentPane(pan);
         this.setVisible(true);
     }
 
-    public void actionPerformed(ActionEvent arg0){
-        this.compteur++;
-        label.setText("Vous avez clique " + this.compteur + " fois");
+    public void actionPerformed(ActionEvent x){
+        if(x.getSource() == bouton) {
+            this.compteur++;
+            label.setText("Vous avez clique " + this.compteur + " fois");
+        }
+        if(x.getSource() == bouton2) {
+            this.compteur--;
+            label.setText("Vous avez clique " + this.compteur + " fois");
+        }
+
+        //getSource permet de déterminer le nom de l'instance qui a généré un évènement
+        //l'argument "x" est un évènement
     }
 
     public static void main(String []args) {
